4f032549f45196ce16199a9bf0fbaf740b628fb8
Kosta <68782786+KostaD02@users.noreply.github.com>
2021-04-30 01:59:06 +0400

    PersonalWorks

--- main.js
+++ main.js
@@ -384,3 +384,25 @@ function reload() {
       }
     });
 }
+function show() {
+  const ipAPI = "//api.ipify.org?format=json";
+  Swal.queue([
+    {
+      title: "Your public IP",
+      confirmButtonText: "Show my public IP",
+      text: "Your public IP will be received " + "via AJAX request",
+      showLoaderOnConfirm: true,
+      preConfirm: () => {
+        return fetch(ipAPI)
+          .then((response) => response.json())
+          .then((data) => Swal.insertQueueStep(data.ip))
+          .catch(() => {
+            Swal.insertQueueStep({
+              icon: "error",
+              title: "Unable to get your public IP",
+            });
+          });
+      },
+    },
+  ]);
+}
